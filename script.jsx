46ae1f9a66e38657f7eaddbff0b5f3d59c76c061
EliseChia <swedesurrender@gmail.com>
2019-08-28 09:10:58 +0800

    output tweet

--- script.jsx
+++ script.jsx
@@ -1,7 +1,39 @@
-var monkey = "hello";
+
+class Tweet extends React.Component {
+
+    render() {
+
+        //based on passing in a single tweet, so later in Tweetfeed class or when rendering then create Tweets in a for loop
+        let userDP = this.props.items.user.profile_image_url;
+        let userName = this.props.items.user.name;
+        let userDisplayName = this.props.items.user.screen_name;
+        let tweetContent = this.props.items.text;
+        let time = this.props.items.created_at;
+        let retweets = this.props.items.retweet_count;
+        let favorites = this.props.items.favorite_count;
+
+
+        return (
+          <div>
+            <img src={userDP}/>
+            <h2>{userName}</h2>
+            <h3>@{userDisplayName}</h3>
+            <p>{tweetContent}</p>
+            <p>Tweeted: {time}</p>
+            <div className="retweet-container">
+                <img src="https://img.icons8.com/material-outlined/48/000000/retweet.png"/>
+                <p>{retweets}</p>
+            </div>
+            <div className="favorite-container">
+                <img src="https://img.icons8.com/material-outlined/48/000000/like.png"/>
+                <p>{favorites}</p>
+            </div>
+          </div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <div>
-      {monkey}
-    </div>,
+    <Tweet items={tweets[0]}/>,
     document.getElementById('root')
 );
